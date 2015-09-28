a12e76628480539fd758926d5eddd7aca0443097
s-gogna <s-gogna@users.noreply.github.com>
2015-09-27 22:11:32 -0700

    Refactor of SymbolTable functions

--- SymbolTable.cpp
+++ SymbolTable.cpp
@@ -8,10 +8,6 @@
 
 SymbolTable::SymbolTable()
 {
-   // Are there any symbols that should be inserted by default?
-   // If so, they should be placed in a map here before
-   // being pushed into the vector
-
    table.push_back(Scope());
 }
 
@@ -47,24 +43,18 @@ SymbolTable::~SymbolTable()
    table.clear();
 }
 
-/**
-@param[in] name String itentifier for the symbol
-@param[in] symbol Data for the symbol
-@return If insert succeeded or not
-**/
-bool SymbolTable::insertSymbol(const string& name, const Symbol& symbol)
+bool SymbolTable::insert(const Symbol& symbol)
 {
-   std::pair<string, Symbol> elemToInsert(name, symbol);
+   std::pair<std::string, Symbol> elemToInsert(symbol.getName(), symbol);
+
+   // TODO
+   // Symbol can be shadowed. Check can be done here and an appropriate
+   // status code can be returned.
 
    table.back().insert(elemToInsert);
 }
 
-/**
-@param[in] name String itentifier for the symbol
-@param[out] symbol Data for the symbol
-@return If lookup succeeded or not
-**/
-bool SymbolTable::findSymbol(const string& name, Symbol& symbol)
+bool SymbolTable::find(const std::string& name, Symbol& symbol)
 {
    std::vector<Scope>::iterator it;
 
@@ -77,26 +67,16 @@ bool SymbolTable::findSymbol(const string& name, Symbol& symbol)
          symbol = elemIter -> second;
          return true;
       }
-
-      // TODO It's possible a variable is being shadowed.
-      // Maybe return an ENUM in that case?
    }
 
    return false;
 }
 
-/**
-@return If a new empty scope was pushed
-**/
-bool SymbolTable::pushScope()
+void SymbolTable::pushScope()
 {
    table.push_back(Scope());
 }
 
-/**
-@param[out] scope Scope containing all identifiers and their Symbols
-@return If a scope was popped or not
-**/
 bool SymbolTable::popScope(Scope& scope)
 {
    if(table.empty()) { return false; }
@@ -107,28 +87,27 @@ bool SymbolTable::popScope(Scope& scope)
    return true;
 }
 
-/**
-@param[in] level Scope level to print
-@return If scope at level exists or not
-**/
-bool SymbolTable::printScope(const size_t& level)
+bool SymbolTable::print(const size_t& level)
 {
-   if(table.empty() || level >= table.size()) { return false; }
+   if(table.empty() || level < 0 || level >= table.size())
+   {
+      return false;
+   }
 
    Scope& scope = table[level];
    Scope::iterator it;
 
+   std::cout << "Level " << level << std::endl;
    for(it = scope.begin(); it != scope.end(); ++it)
    {
-      cout << (it -> first) << ": " << (it -> second) << endl;
+      std::cout << "  " << (it -> first)
+         << ": " << (it -> second)
+         << std::endl;
    }
 
    return true;
 }
 
-/**
-@return Total number of levels (scopes) in the table
-**/
 size_t SymbolTable::size()
 {
    return table.size();
