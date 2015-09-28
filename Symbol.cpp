ffbcdee1a14511981748648bfb76c4124223ad99
s-gogna <s-gogna@users.noreply.github.com>
2015-09-27 21:13:11 -0700

    Started Symbol implementation

---
+++ Symbol.cpp
@@ -0,0 +1,50 @@
+/******************************************************************************
+@file Symbol.cpp
+******************************************************************************/
+#ifndef SYMBOL_IMPL
+#define SYMBOL_IMPL
+
+#include "Symbol.hpp"
+
+Symbol::Symbol(const Symbol& src)
+{
+	identifier = src.identifier;
+}
+
+const Symbol& Symbol::operator=(const Symbol& src)
+{
+	if(&src == this) { return src; }
+
+	identifier = src.identifier;
+}
+
+Symbol::~Symbol()
+{
+	identifier.clear();
+}
+
+/**
+@param[in] name String itentifier for the symbol
+@param[in] symbol Data for the symbol
+@return If insert succeeded or not
+**/
+bool Symbol::setName(const std::string& name)
+{
+	identifier = name;
+}
+
+/**
+@return Identifier
+**/
+const std::string& Symbol::getName()
+{
+	return identifier;
+}
+
+std::ostream& operator<<(std::ostream& stream, const Symbol& symbol)
+{
+	stream << symbol.identifier;
+	return stream;
+}
+
+#endif
