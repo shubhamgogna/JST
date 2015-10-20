1143e4c91a9b146458458283c5180e450740259b
Shubham K Gogna <s-gogna@users.noreply.github.com>
2015-10-20 13:38:35 -0700

    Started separate symbol table driver

---
+++ bin/symbol_table_driver.py
@@ -0,0 +1,42 @@
+# This file is part of JST.
+#
+# JST is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# JST is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with JST.  If not, see <http://www.gnu.org/licenses/>.
+
+from symbol_table.symbol_table import SymbolTable
+from symbol_table.symbol import Symbol
+
+def main():
+    sym = SymbolTable()
+
+    sym.insert(Symbol('Apple'))
+    sym.insert(Symbol('Banana'))
+    sym.push()
+    sym.insert(Symbol('Cantaloupe'))
+    print(sym)
+
+    assert sym.size() is 2
+
+    found = sym.find('Apple')
+    assert found is not None
+    assert found.identifier is 'Apple'
+
+    found = sym.find('Cantaloupe')
+    assert found is not None
+    assert found.identifier is 'Cantaloupe'
+
+    found = sym.find('Durian')
+    assert found is None
+
+if __name__ == '__main__':
+    main()
