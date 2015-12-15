64fb972838296e16cb9f013673d0dd4d32d1b4f1
Janelle Blankenburg <jjblankenburg@gmail.com>
2015-12-14 21:21:44 -0800

    Added test case for array stuffs, very simple but shows assignment on left and right hand sides. It's working in MARS yay!

--- mips/test/test_full_run_assembly_generation.py
+++ mips/test/test_full_run_assembly_generation.py
@@ -328,5 +328,44 @@ class TestFullRunAssemblyGeneration(unittest.TestCase):
         self.generator.translate_tac_to_mips()
         print(self.generator.dumps())
 
+    def test_array_declarations_and_manipulation(self):
+        data = """
+            int main() {
+
+                int i[3];
+                int j[2][2];
+                int k[2][2][2][2][2];
+                int s;
+
+                // 1-D manipulation
+                i[0] = 2;
+                i[2] = i[0];
+                for( s = 0; s < 3; s++) {
+                    print_int(i[s]);
+                }
+
+                // 2-D manipulation
+                j[0][0] = 20;
+                print_int(j[0][0]);
+                j[1][1] = j[0][0];
+                print_int(j[1][1]);
+
+
+                // 5-D manipulation
+                k[1][0][1][0][1] = 45;
+                print_int(k[1][0][1][0][1]);
+                k[0][0][0][0][1] = k[1][0][1][0][1];
+                print_int(k[0][0][0][0][1]);
+
+
+                return 0;
+            }
+            """
+        ast = self.compiler_state.parse(data)
+        source_tac = ast.to_3ac()
+
+        self.generator.load(source_tac)
+        self.generator.translate_tac_to_mips()
+        print(self.generator.dumps())
 
 
