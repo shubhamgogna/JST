f005621938d3f5c88b6017d737213d3a2733cd5d
Shubham Gogna <s-gogna@users.noreply.github.com>
2015-12-16 01:16:52 -0800

    Update bubble sort test

--- mips/test/test_full_run_assembly_generation.py
+++ mips/test/test_full_run_assembly_generation.py
@@ -727,8 +727,7 @@ class TestFullRunAssemblyGeneration(unittest.TestCase):
             const int N_ITEMS = 5;
 
             int main() {
-              int i, j;
-              int temp;
+              int i, j, temp;
             // int things[N_ITEMS] = {5, 1, 4, 3, 2};
 
               int things[N_ITEMS];
@@ -739,10 +738,15 @@ class TestFullRunAssemblyGeneration(unittest.TestCase):
               things[4] = 2;
 
               print_int(things[0]);  // expect to see 5
+              print_char('\\n');
               print_int(things[1]);  // expect to see 1
+              print_char('\\n');
               print_int(things[2]);  // expect to see 4
+              print_char('\\n');
               print_int(things[3]);  // expect to see 3
+              print_char('\\n');
               print_int(things[4]);  // expect to see 2
+              print_char('\\n');
 
               for (i = 0; i < N_ITEMS; i++) {
                 for (j = i; j < N_ITEMS; j++) {
@@ -754,10 +758,15 @@ class TestFullRunAssemblyGeneration(unittest.TestCase):
                 }
               }
 
+              print_char('\\n');
               print_int(things[0]);  // expect to see 5
+              print_char('\\n');
               print_int(things[1]);  // expect to see 4
+              print_char('\\n');
               print_int(things[2]);  // expect to see 3
+              print_char('\\n');
               print_int(things[3]);  // expect to see 2
+              print_char('\\n');
               print_int(things[4]);  // expect to see 1
 
               return 0;
@@ -765,6 +774,7 @@ class TestFullRunAssemblyGeneration(unittest.TestCase):
             """
         ast = self.compiler_state.parse(data)
         source_tac, i = ast.to_3ac()
+        print(i);
 
         self.generator.load(source_tac)
         self.generator.translate_tac_to_mips()
@@ -861,6 +871,7 @@ class TestFullRunAssemblyGeneration(unittest.TestCase):
               int x = 5;
               int result = factorial(x);
               print_int(x);       // expect to see 5
+              print_char('\\n');
               print_int(result);  // expect to see 120
 
               return 0;
@@ -876,6 +887,7 @@ class TestFullRunAssemblyGeneration(unittest.TestCase):
             """
         ast = self.compiler_state.parse(data)
         source_tac, i = ast.to_3ac()
+        print(i)
 
         self.generator.load(source_tac)
         self.generator.translate_tac_to_mips()
