0a3249aa1ab983e9803e1e7f3d991be04a1c28b4
Shubham Gogna <s-gogna@users.noreply.github.com>
2015-12-15 23:32:59 -0800

    Started adding MIPS parts

---
+++ javascripts/expandable.js
@@ -0,0 +1,14 @@
+/**
+ * Adapted from example at
+ * http://jsfiddle.net/hungerpain/eK8X5/7/
+ **/
+function makeCollapseable(object) {
+  container = $(object);
+  container.children(":first").click(createToggleCallback(container));
+}
+
+function createToggleCallback(parent) {
+  return function() {
+    parent.children(".content").slideToggle(500);
+  }
+}
